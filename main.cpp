d2e94a2e9b60aec94619e493682d8ca23ee2a856
Arkadzi Zaleuski <arkansweet@gmail.com>
2021-11-25 03:28:43 +0100

    Optimized space(2 times) and operation count for single threaded implementation

--- main.cpp
+++ main.cpp
@@ -2,38 +2,53 @@
 #include <cmath>
 #include <algorithm>
 #include <chrono>
+#include <vector>
+
 using namespace std::chrono;
 using namespace std;
 
-int fun(int n, bool* tab) {
-	int k, wynik = 0;
-	for (int i = 2; i <= sqrt(n); i++)
-		if (tab[i])
-			for (int j = 2; (k = j * i) <= n; j++)
-				tab[k] = false;
-	for (int i = 2; i <= n; i++)
+int offset = 3;
+int getNum(int index){
+    return 2*index + offset;
+}
+
+int getIndex(int number){
+    return (number-offset)/2;
+}
+
+vector<int> fun(int n, bool* tab) {
+    vector<int> result;      result.push_back(1);    result.push_back(2);
+    int upperBound = getIndex(static_cast<int>(sqrt(n)));
+	for (int i = 0; i <=upperBound; i++)
 		if (tab[i]) {
-			wynik++;
-			cout << i << ", ";
+            int number = getNum(i);
+            result.push_back(number);
+            for (int j = getIndex(number*number); j < getIndex(n); j+=number)
+                tab[j] = false;
+        }
+	for (int i = upperBound + 1; i <= getIndex(n); i++)
+		if (tab[i]) {
+            result.push_back(getNum(i));
 		}
-	return wynik;
+	return result;
 }
 
 int main() {
-	int n, wynik;
+	int n;
 	cout << "Podaj n: ";
-	cin >> n;
+	cin >> n ;
 	bool* tab;
-	//cudaMallocManaged(&tab, n * sizeof(bool));
-	tab = new bool[n + 1];
-	fill_n(tab, n, true);
+	tab = new bool[getIndex(n)+1];  //replace with array of bits
+	fill_n(tab, getIndex(n), true);
 	auto start = high_resolution_clock::now();
-	wynik = fun(n, tab);
+    vector<int> result = fun(n, tab);
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
-	cout << "Czas: " << duration.count() << endl;
-	cout << "Liczb pierwszych w zakresie [2, n] jest: " << wynik;
-	//cudaFree(tab);
+    for(auto const& value: result){
+        cout << value << " ";
+    }
+	cout << "\nCzas: " << duration.count() << " microsekund." << endl;
+	cout << "Liczb pierwszych w zakresie [2, n] jest: " << result.size();
 	delete [] tab;
 	return 0;
 }
